1f15cc34f2ccf34924b1b20aa713e8785d4dea99
Jean-Marie Pivette <46813298+JMPivette@users.noreply.github.com>
2024-07-08 11:02:22 +0200

    fix left footer not being aligned

--- R/utils_tabler.R
+++ R/utils_tabler.R
@@ -332,3 +332,39 @@ tablerDashPage <- function(navbar = NULL, body = NULL,
     )
   )
 }
+
+#' Create a Boostrap 4 dashboard footer. Modification version of 
+#'
+#' Build an adminLTE3 dashboard footer
+#'
+#' @param ... Left text.
+#' @param copyrights Copyrights, if any.
+#'
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @export
+tablerDashFooter <- function(..., copyrights = NULL) {
+
+  shiny::tags$footer(
+    class = "footer",
+    shiny::tags$div(
+      class = "container",
+      shiny::tags$div(
+        class = "row align-items-center",
+        # copyright left
+        shiny::tags$div(
+          class = "col-12 col-lg-auto mt-3 mt-lg-0 text-center",
+          copyrights
+        ),
+        # right elements
+        shiny::tags$div(
+          class = "col-auto ml-lg-auto",
+          shiny::tags$div(
+            class = "row align-items-center",
+            lapply(list(...), shiny::tagAppendAttributes, class = "mx-2")
+          )
+        )
+      )
+    )
+  )
+}
